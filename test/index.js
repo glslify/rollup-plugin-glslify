e383b213db5a4d0f20d355331236108ce65517f8
Roman <r89534217610@gmail.com>
2022-06-16 18:34:49 +0400

    allow supplying custom compress function for options

--- test/index.js
+++ test/index.js
@@ -18,6 +18,20 @@ test('compressed', assert => rollup.rollup({
     assert.end();
 }));
 
+test('compressed | custom function', assert => rollup.rollup({
+    input: 'fixtures/basic.js',
+    plugins: [glslify({ compress: (code) => code + '\n\n// test custom function' })]
+}).then(bundle => bundle.generate({ format: 'es' })).then(generated => {
+    const code = generated.output[0].code;
+    assert.true(code.includes('// Description'));
+    assert.true(code.includes('taylorInvSqrt'));
+    assert.true(code.includes('// test custom function'));
+    assert.end();
+}).catch(err => {
+    assert.error(err);
+    assert.end();
+}));
+
 test('uncompressed', assert => rollup.rollup({
     input: 'fixtures/basic.js',
     plugins: [glslify({ compress: false })]
